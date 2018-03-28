98ee3b779a12e5536197f01e994f531715923f20
sharbel93 <sharbel.chris@gmail.com>
2018-03-28 14:51:05 +0300

    initial commit, with activities

---
+++ app/src/main/java/com/example/geek_sharbel/hogapp/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.geek_sharbel.hogapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
